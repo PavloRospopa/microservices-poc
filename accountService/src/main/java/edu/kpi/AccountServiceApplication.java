8a86bbcd8a3a5b48e059af20673644149b9e26a7
Wayruha <rwdiachuk@gmail.com>
2018-04-18 23:11:22 +0300

    init for Eureka server. No connection between  two services for now

--- accountService/src/main/java/edu/kpi/AccountServiceApplication.java
+++ accountService/src/main/java/edu/kpi/AccountServiceApplication.java
@@ -2,13 +2,15 @@ package edu.kpi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
-public class SprBootMicroservicesApplication {
+@EnableEurekaServer
+public class AccountServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SprBootMicroservicesApplication.class, args);
+        SpringApplication.run(AccountServiceApplication.class, args);
     }
 }
