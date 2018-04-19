04b8c46f1782a7f02cd76686575b1735c6ab5f52
Wayruha <rwdiachuk@gmail.com>
2018-04-19 19:37:22 +0300

    EUREKA

--- accountService/src/main/java/edu/kpi/AccountServiceApplication.java
+++ accountService/src/main/java/edu/kpi/AccountServiceApplication.java
@@ -2,12 +2,12 @@ package edu.kpi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
-@EnableEurekaServer
+@EnableEurekaClient
 public class AccountServiceApplication {
 
     public static void main(String[] args) {
